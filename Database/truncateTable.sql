b7f415bd67feddae64fabdc3c54712c3cf075fe2
Gia Bảo Hoàng <61697588+baohoang2812@users.noreply.github.com>
2020-07-09 23:27:56 +0700

    Regenerate no error database script and add TruncateTable script

---
+++ Database/truncateTable.sql
@@ -0,0 +1,12 @@
+use SiteKick
+go
+TRUNCATE TABLE [dbo].[SiteTech];
+go
+TRUNCATE TABLE [dbo].[Site];
+go
+TRUNCATE TABLE [dbo].[Category];
+go
+TRUNCATE TABLE [dbo].[Technology];
+go
+TRUNCATE TABLE [dbo].[TechnologyGroup];
+go
